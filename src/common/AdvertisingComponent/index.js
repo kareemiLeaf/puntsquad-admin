26383611b98ef609f5c670398fd77ef718670eab
Linu <linu.s@ileafsolutions.net>
2022-05-24 10:45:51 +0530

    Advertisement screen UI updated

--- src/common/AdvertisingComponent/index.js
+++ src/common/AdvertisingComponent/index.js
@@ -1,7 +1,18 @@
+import "./AdvertisingComponent.module.scss";
+
 import { CameraFilled, SaveOutlined } from "@ant-design/icons";
-import { Button, Col, Dropdown, Menu, message, Row, Upload } from "antd";
+import {
+  Button,
+  Col,
+  DatePicker,
+  message,
+  Row,
+  TimePicker,
+  Upload,
+} from "antd";
 import axios from "axios";
 import { useFormik } from "formik";
+import moment from "moment";
 import { useState } from "react";
 import { getToken } from "utils";
 import { BASE_URL } from "utils/constants";
@@ -10,22 +21,34 @@ import * as Yup from "yup";
 const TipSchema = Yup.object().shape({
   link: Yup.string()
     .matches(
-      /^((ftp|http|https):\/\/)?(www.)?(?!.*(ftp|http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+((\/)[\w#]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?$/gm,
+      /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\\/]))?/,
       "Enter valid url!"
     )
     .required("Please enter url"),
 });
 
+// function readFile(file) {
+//   return new Promise((resolve) => {
+//     const reader = new FileReader();
+//     reader.addEventListener("load", () => resolve(reader.result), false);
+//     reader.readAsDataURL(file);
+//   });
+// }
+
 function AdvertisingComponent({ styles, getAds }) {
   const [img, setImg] = useState(null);
   const [loading, setLoading] = useState(false);
-  const [hour, setHour] = useState("4 Hours");
+  const [hour, setHour] = useState(moment().format("HH:MM:ss"));
+  const [date, setDate] = useState(new Date());
 
   const handleValue = async (values, actions) => {
     setLoading(true);
     const data = new FormData();
     data.append("image", img);
-    data.append("duration", hour);
+    data.append(
+      "duration",
+      `${moment(date).format("YYYY-MM-DD")} ${moment(hour).format("HH:MM:ss")}`
+    );
     data.append("link", values.link);
     const config = {
       method: "post",
@@ -36,6 +59,7 @@ function AdvertisingComponent({ styles, getAds }) {
       },
       data: data,
     };
+
     axios(config)
       .then(function (response) {
         console.log("RESPONSE", response.data);
@@ -69,22 +93,7 @@ function AdvertisingComponent({ styles, getAds }) {
   const handleRemoveUser = () => {
     formik.resetForm();
   };
-  const handleClick = ({ key }) => {
-    console.log("KEY", key);
-    setHour(key);
-    //you can perform setState here
-  };
 
-  const menu = (
-    <Menu onClick={handleClick}>
-      <Menu.Item key="4 Hours">4 Hours</Menu.Item>
-      <Menu.Item key="8 Hours">8 Hours</Menu.Item>
-      <Menu.Item key="8 Hours">12 Hours</Menu.Item>
-      <Menu.Item key="16 Hours">16 Hours</Menu.Item>
-      <Menu.Item key="16 Hours">20 Hours</Menu.Item>
-      <Menu.Item key="24 Hours">24 Hours</Menu.Item>
-    </Menu>
-  );
   return (
     <>
       <div className={styles.wrapper}>
@@ -93,24 +102,48 @@ function AdvertisingComponent({ styles, getAds }) {
           <Col span={14}>
             <Row justify="space-between" gutter={10}>
               <Col span={24}>
-                <p className={styles.label}>Advertising Duration</p>
+                <p className={styles.label}>Advertising duration</p>
 
-                <Dropdown
-                  className={styles.input}
-                  overlayClassName={styles.input}
-                  overlay={menu}
-                >
-                  <Button style={{ color: "white", textAlign: "left" }}>
-                    {hour}
-                  </Button>
-                </Dropdown>
+                <Row justify="space-between" gutter={10}>
+                  <Col className="mt-3" span={10}>
+                    <p className={styles.label}>Advertising Date</p>
+                    <DatePicker
+                      // defaultValue={moment()}
+                      // format={"YYYY/MM/DD"}
+                      disabledDate={(current) => {
+                        let customDate = moment().format("YYYY-MM-DD");
+                        return (
+                          current && current < moment(customDate, "YYYY-MM-DD")
+                        );
+                      }}
+                      // value={moment(date).format("YYYY-MM-DD")}
+                      onChange={(e) => setDate(e)}
+                      className={styles.input1}
+                    />
+
+                    {!date && <pre className="text-danger">{"Enter Date"}</pre>}
+                  </Col>
+                  <Col className="mt-3" span={10}>
+                    <p className={styles.label}>Advertising Time</p>
+                    <div className={styles.input2}>
+                      <TimePicker
+                        use12Hours
+                        format="h:mm a"
+                        className="p-0"
+                        bordered={false}
+                        onChange={(e) => setHour(moment(e))}
+                      />
+                    </div>
+                    {!hour && <pre className="text-danger">{"Enter Time"}</pre>}
+                  </Col>
+                </Row>
               </Col>
 
               <Col span={24} className="my-3">
                 <p className={styles.label}>Hyperlink for advertising</p>
                 <textarea
                   placeholder="Enter url"
-                  className={styles.input}
+                  className={styles.input1}
                   rows={10}
                   onChange={formik.handleChange("link")}
                   onBlur={formik.handleBlur("link")}
@@ -139,6 +172,7 @@ function AdvertisingComponent({ styles, getAds }) {
             <div className={styles.imgWrap}>
               {img && <img src={URL.createObjectURL(img)} />}
             </div>
+
             <div className="d-flex justify-content-center mt-4 flex-column align-items-center w-100">
               <Upload
                 showUploadList={false}
@@ -149,7 +183,7 @@ function AdvertisingComponent({ styles, getAds }) {
                     message.error("Please choose a image size less than 2MB");
                   }
                 }}
-                onChange={(e) => {
+                onChange={async (e) => {
                   if (e?.file?.size < 2097152) {
                     setImg(e?.file?.originFileObj);
                   }
